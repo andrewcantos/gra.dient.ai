51f91e788f081f19652a52ee84cec36d697d6404
Andrew Cantos <andrewcantos@gmail.com>
2025-04-13 06:34:03 +0000

    Disable purging in Tailwind config to silence warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./styles/**/*.{css,scss}"
   ],
+  purge: false, // Explicitly disable purging to silence warnings
   theme: {
     extend: {},
   },
